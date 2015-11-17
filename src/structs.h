66eb84f196f71f58c3cab06675b26bea45ccb0e2
andersjkbsn <andersjkbsn@gmail.com>
2015-11-17 10:04:22 +0100

    added comments

--- src/structs.h
+++ src/structs.h
@@ -1,3 +1,4 @@
+/* Lecture struct */
 typedef struct
 {
 	unsigned int day;
@@ -7,18 +8,21 @@ typedef struct
 	room* assignedRoom;
 } lecture;
 
+/* Room struct */
 typedef struct
 {
 	char[32] name;
 	unsigned int size;
 } room;
 
+/* Teacher struct */
 typedef struct
 {
 	char[32] name;
 	unsigned int* offDays;
 } teacher;
 
+/* Course struct */
 typedef struct
 {
 	char[32] name;
