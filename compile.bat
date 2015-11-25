ba98e0a330a2c53c23ea5485343abaee51dba867
andersjkbsn <andersjkbsn@gmail.com>
2015-11-25 13:17:14 +0100

    added initial data test

--- compile.bat
+++ compile.bat
@@ -4,9 +4,13 @@ mkdir obj
 :: Compile data utility object
 gcc -g -c -ansi -pedantic -Wall src/data_utility.c -o obj/data_utility.o
 
+:: Compile data test object
+gcc -g -c -ansi -pedantic -Wall src/data_test.c -o obj/data_test.o
+
 :: Compile config reader object
 gcc -g -c -ansi -pedantic -Wall src/config_reader.c -o obj/config_reader.o
 
 :: Compile executable
-gcc -g -ansi -pedantic -Wall src/scheduler.c obj/config_reader.o obj/data_utility.o -o bin/Scheduler.exe
+gcc -g -ansi -pedantic -Wall src/scheduler.c obj/data_test.o obj/config_reader.o obj/data_utility.o -o bin/Scheduler.exe
+
 pause
