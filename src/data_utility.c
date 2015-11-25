b919e892640cf03dc520cb73e35f2c06e0441907
andersjkbsn <andersjkbsn@gmail.com>
2015-11-25 10:00:08 +0100

    added check for index exceeding array bounds

--- src/data_utility.c
+++ src/data_utility.c
@@ -1,31 +1,46 @@
 #include "structs.h"
-/**  **/
-/* This function returns the room specified by the index parameter */
+
+/* Returns the room specified by the index parameter */
 room get_room(semesterData *sd, int index)
 {
-    return sd->rooms[index];
+    if (index < 0 || index >= sd->numTeachers)
+        return NULL;
+    else
+        return sd->teachers[index];
 }
 
-/* This function returns the teacher specified by the index parameter */
+/* Returns the teacher specified by the index parameter */
 teacher get_teacher(semesterData *sd, int index)
 {
-    return sd->teachers[index];
+    if (index < 0 || index >= sd->numTeachers)
+        return NULL;
+    else
+        return sd->teachers[index];
 }
 
-/* This function returns the course specified by the index parameter */
+/* Returns the course specified by the index parameter */
 course get_course(semesterData *sd, int index)
 {
-    return sd->courses[index];
+    if (index < 0 || index >= sd->numCourses)
+        return NULL;
+    else
+        return sd->courses[index];
 }
 
-/* This function returns the specialization specified by the index parameter */
+/* Returns the specialization specified by the index parameter */
 specialization get_specialization(semesterData *sd, int index)
 {
-    return sd->specializations[index];
+    if (index < 0 || index >= sd->numSpecializations)
+        return NULL;
+    else
+        return sd->specializations[index];
 }
 
-/* This function returns the lecture specified by the index parameter */
+/* Returns the lecture specified by the index parameter */
 lecture get_lecture(semesterData *sd, int index)
 {
-    return sd->lectures[index];
+    if (index < 0 || index >= sd->numLectures)
+        return NULL;
+    else
+        return sd->lectures[index];
 }
