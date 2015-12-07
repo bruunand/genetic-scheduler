6d098c99fa88fb995d1ea5439979079db24336d1
tj45794 <jendal@live.dk>
2015-12-07 10:39:43 +0100

    also test for lectures pr week

--- src/unimplemented/test_distribution.c
+++ src/unimplemented/test_distribution.c
@@ -1,7 +1,7 @@
 
 void test_distribution(semesterData *sd)
 {
-    int i, courseNum, weekNum, dayNum, totCoursePerWeek, totCoursePerDay, totDays, severity = 0;
+    int i, courseNum, weekNum, dayNum, totCoursePerWeek, totCoursePerDay, totLectPerWeek, totDays, severity = 0;
   
     totDays = numWeeks * DAYS_PER_WEEK;
     
@@ -11,14 +11,18 @@ void test_distribution(semesterData *sd)
             if (dayNum / DAYS_PER_WEEK > weekNum)
                 weekNum++;
             
-            for (i = 0, totCoursePerWeek = 0, totCoursePerDay = 0; i < sd->numLectures); i++)
+            for (i = 0, totCoursePerWeek = 0, totCoursePerDay = 0, totLectPerWeek = 0; i < sd->numLectures); i++)
             {
+                if (sd->lectures[i].day / DAYS_PER_WEEK == weekNum && sd->lectures[i].assignedCourse == 0 && sd->lectures[i].assignedCourse)
+                    totLectPerWeek++;
                 if (sd->lectures[i].day / DAYS_PER_WEEK == weekNum && sd->lectures[i].assignedCourse == courseID)
                    totCoursePerWeek++; 
                 if (sd->lectures[i].day == dayNum && sd->lectures[i].assignedCourse == courseID)
                     totCoursePerDay++;
             }
             
+            if (totLectPerWeek > 7)
+                severity += totLectPerWeek - 7;
             if (totCoursePerWeek > 3)
                 severity += totCoursePerWeek - 3;
             if (totCoursePerDay > 1)
