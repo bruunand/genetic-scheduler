0e7917fd12387bed200133e94b370291fa326eb5
BullDuckling <tbuhla@gmail.com>
2015-12-07 09:48:53 +0100

    comment expanded

--- src/unimplemented/crossbreed.c
+++ src/unimplemented/crossbreed.c
@@ -5,7 +5,7 @@ int crossbreed(semesterData *parent1, semesterData *parent2, semesterData *child
     child1 = parent1;
     child2 = parent2;   
     
-    /* For a random selection of lectures, they are swi */
+    /* For a random selection of lectures, they are assigned from the other parent */
     for(i = 0;i < parent1->numLectures;i += rand() % (parent1->numLectures/10)) 
     {
         child1->lectures[i] = parent2->lectures[i];
