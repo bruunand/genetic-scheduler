73a5b92b1c82084740ae37e6cdb2c6ce81756c43
BullDuckling <tbuhla@gmail.com>
2015-12-09 11:31:42 +0100

    crosbreed WORK IN PROGRESS

--- src/unimplemented/crossbreed.c
+++ src/unimplemented/crossbreed.c
@@ -1,16 +1,22 @@
-int crossbreed(semesterData *parent1, semesterData *parent2, semesterData *child1, semesterData *child2)
+int crossbreed(Generation *old, Generation *new)
 {
-    int i, genesSwitched = 0;
+    int i, j, genesSwitched = 0;
     
-    child1 = parent1;
-    child2 = parent2;   
-    
-    /* For a random selection of lectures, they are assigned from the other parent */
-    for(i = 0;i < parent1->numLectures;i += rand() % (parent1->numLectures/10)) 
-    {
-        child1->lectures[i] = parent2->lectures[i];
-        child2->lectures[i] = parent1->lectures[i];
-        genesSwitched++;
+    /*  */
+    for (i = (GENERATION_SIZE / 10) - 1; i < GENERATION_SIZE - 10; i++)
+    {    
+        newGen[i] = curGen[i];
+        
+        for(j = 0; j < newGen[i].numLectures ; j++)
+        {
+            /* Skip perfect lectures */
+            if (newGen[i].lectures[j].fitness == 0)
+                continue;
+            if(newGen[i].lectures[j].fitness < curGen[i + 1 - (2 * (i % 2))].lectures[j].fitness){
+                newGen[i].lectures[j] = curGen[].lectures[j];
+                genesSwitched++;
+            }
+            
     }
     
     return genesSwitched;
