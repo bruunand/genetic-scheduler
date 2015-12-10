b5563395618b0132fd9679679b7f7e5d5099327e
BullDuckling <tbuhla@gmail.com>
2015-12-10 13:43:54 +0100

    possible mutation
    Mutates a random number of lectures.
    50% chance for mutaition of Room, Day & Period.

---
+++ src/unimplemented/mutation.c
@@ -0,0 +1,28 @@
+int crossbreed(Generation *new, int genomeId)
+{
+    int i = 0, mutations;
+    
+    do
+    {
+        i += rand() % new->sd->numLectures;
+        if(!(rand() % 2))
+        {
+            new->schedules[genomeId].lectures[i].day = rand() % (sd->numWeeks * DAYS_PER_WEEK);
+            mutations++;
+        }
+
+        if(!(rand() % 2))
+        {
+            new->schedules[genomeId].lectures[i].period = rand() % MAX_PERIODS;
+            mutations++;
+        }
+      
+        if(!(rand() % 2))
+        {
+            new->schedules[genomeId].lectures[i].assignedRoom = &sd->rooms[rand() % sd->numRooms];
+            mutations++;
+        }
+    } while(i < new->sd->numLectures);
+
+    return genesSwitched;
+}
